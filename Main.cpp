3ddb196f77b136ca5f35b99f8885089c1215023f
Gab <shytix1202@gmail.com>
2020-11-26 19:49:29 +0100

    Création d'une texture + d'un sprite pour remplacer la balle du débout par un ballon de basket. Suppression de la balle rouge du début. Documentation général

--- Main.cpp
+++ Main.cpp
@@ -1,25 +1,28 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
 
 using namespace sf;
 
-void updateBall(CircleShape& ball, RenderWindow& window, bool& isShot);
+// Prototype
+void updateBall(Sprite& ballSprite, RenderWindow& window, bool& isShot);
 void updateHoop(CircleShape& hoop, RenderWindow& window, int& dir, float& hoopSpeed);
-void collisionBall(CircleShape& ball, CircleShape& hoop, RenderWindow& window, bool& isShot, int& points, Text& textPoints);
+void collisionBall(Sprite& ballSprite, CircleShape& hoop, RenderWindow& window, bool& isShot, int& points, Text& textPoints);
 void changeDifficulty(int& points, float& hoopSpeed);
 
 int main()
 {
+	// Some variables
 	bool isShot{ false };
 	float hoopSpeed{ 5.f };
 	int dir{ 0 };
 	int points{ 0 };
 
+	// Creation of the window
     RenderWindow window(sf::VideoMode(640, 480), "Simple Square Game");
 	window.setMouseCursorVisible(false);
 	window.setFramerateLimit(60);
 
+	// Creation of the hoop
 	CircleShape hoop;
 	hoop.setRadius(50.f);
 	hoop.setFillColor(Color::Black);
@@ -27,15 +30,12 @@ int main()
 	hoop.setOutlineColor(Color::Blue);
 	hoop.setPosition(Vector2f(0, 10.f));
 
-	CircleShape ball;
-	ball.setRadius(20.f);
-	ball.setFillColor(Color::Red);
-	ball.setPosition(Vector2f(0, window.getSize().y - ball.getRadius()*3));
-
+	// Loading of the front
 	Font font;
 	if (!font.loadFromFile("Fonts/Almond Nougat.ttf"))
 		throw("Impossible de charger la police.");
 
+	// Creation of the "textPoints"
 	Text textPoints;
 	textPoints.setFont(font);
 	textPoints.setCharacterSize(24);
@@ -43,6 +43,18 @@ int main()
 	textPoints.setStyle(Text::Bold);
 	textPoints.setPosition(5.f, 0.f);
 	textPoints.setString("Points : " + std::to_string(points));
+
+	// Creation of the "ballTexture"
+	Texture ballTexture;
+	if (!ballTexture.loadFromFile("Images/basketball.png"))
+		throw("Erreur: impossible de charger l'image.");
+
+	ballTexture.setSmooth(true);
+
+	// Creation of the "ballSprite"
+	Sprite ballSprite;
+	ballSprite.setTexture(ballTexture);
+	ballSprite.setPosition(Vector2f(0, window.getSize().y - 80));
 
 
 	while (window.isOpen())
@@ -61,10 +73,10 @@ int main()
 		updateHoop(hoop, window, dir, hoopSpeed);
 
 		// Update ball
-		updateBall(ball, window, isShot);
+		updateBall(ballSprite, window, isShot);
 
 		// Collision ball
-		collisionBall(ball, hoop, window, isShot, points, textPoints);
+		collisionBall(ballSprite, hoop, window, isShot, points, textPoints);
 
 		// Change difficulty
 		changeDifficulty(points, hoopSpeed);
@@ -72,8 +84,9 @@ int main()
 		// Draw
 		window.clear(Color::White);
 
+		window.draw(ballSprite);
 		window.draw(hoop);
-		window.draw(ball);
+		// window.draw(ball);
 		window.draw(textPoints);
 
 		window.display();
@@ -83,15 +96,16 @@ int main()
     return 0;
 }
 
-void updateBall(CircleShape& ball, RenderWindow& window, bool& isShot)
+
+void updateBall(Sprite& ballSprite, RenderWindow& window, bool& isShot)
 {
 	if (Mouse::isButtonPressed(Mouse::Left))
 		isShot = true;
 
 	if (!isShot)
-		ball.setPosition(Mouse::getPosition(window).x, ball.getPosition().y);
+		ballSprite.setPosition(Mouse::getPosition(window).x, ballSprite.getPosition().y);
 	else
-		ball.move(0, -5.f);
+		ballSprite.move(0, -5.f);
 }
 
 void updateHoop(CircleShape& hoop, RenderWindow& window, int& dir, float& hoopSpeed)
@@ -113,19 +127,21 @@ void updateHoop(CircleShape& hoop, RenderWindow& window, int& dir, float& hoopSp
 	}
 }
 
-void collisionBall(CircleShape& ball, CircleShape& hoop, RenderWindow& window, bool& isShot, int& points, Text& textPoints)
+void collisionBall(Sprite& ballSprite, CircleShape& hoop, RenderWindow& window, bool& isShot, int& points, Text& textPoints)
 {
 
-	if (ball.getGlobalBounds().intersects(hoop.getGlobalBounds()) || ball.getPosition().y < 0)
+	if (ballSprite.getGlobalBounds().intersects(hoop.getGlobalBounds()) || ballSprite.getPosition().y < -10)
 	{
-		if (ball.getGlobalBounds().intersects(hoop.getGlobalBounds()))
+
+		// Si la condition est respecté on ajoute un point.
+		if (ballSprite.getGlobalBounds().intersects(hoop.getGlobalBounds()))
 		{
 			points++;
 			textPoints.setString("Points : " + std::to_string(points));
 		}
 
 		isShot = false;	
-		ball.setPosition(ball.getPosition().x, window.getSize().y - ball.getRadius() * 3);
+		ballSprite.setPosition(ballSprite.getPosition().x, window.getSize().y - 80);
 
 	}
 }
